8f3a21e26f7f01cf92d55b478898a21ba6d86427
Tony Tran <toant713@gmail.com>
2014-04-13 10:30:22 -0700

    viewset way and automatically creating api url

--- DjangoApps/RestApp/simplepastebin/views.py
+++ DjangoApps/RestApp/simplepastebin/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, reverse, renderers
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, reverse, renderers, viewsets
+from rest_framework.decorators import api_view, link
 from rest_framework.response import Response
 from simplepastebin.models import Snippet
 from simplepastebin.permissions import IsOwnerOrReadOnly
@@ -9,47 +9,69 @@ from simplepastebin.serializers import SnippetSerializer, UserSerializer
 
 
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
-    def pre_save(self, obj):
-        obj.owner = self.request.user
+# class SnippetList(generics.ListCreateAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#     
+#     def pre_save(self, obj):
+#         obj.owner = self.request.user
+# 
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+#                       IsOwnerOrReadOnly,)
+#     
+#     def pre_save(self, obj):
+#         obj.owner = self.request.user
+#         
+# class SnippetHighlight(generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     renderer_classes = (renderers.StaticHTMLRenderer,)
+# 
+#     def get(self, request, *args, **kwargs):
+#         snippet = self.get_object()
+#         return Response(snippet.highlighted) 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                      IsOwnerOrReadOnly,)
-    
-    
+                          IsOwnerOrReadOnly,)
+
+    @link(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
     def pre_save(self, obj):
         obj.owner = self.request.user
+
     
-class UserList(generics.ListAPIView):
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+# 
+# 
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
     
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
 
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted) 
  
  
-    
-@api_view(('GET',))
-def api_root(request, format=None):
-    if request.method == 'GET':
-        return Response({
-            'users': reverse('user-list', request=request, format=format),
-            'snippets': reverse('snippet-list', request=request, format=format)
-        })
+
