1c36b1fb8630483218033f2c55fa513e5761e7be
Tony Tran <toant713@gmail.com>
2014-04-13 10:33:48 -0700

    removed commented out code

--- DjangoApps/RestApp/simplepastebin/views.py
+++ DjangoApps/RestApp/simplepastebin/views.py
@@ -1,38 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, reverse, renderers, viewsets
-from rest_framework.decorators import api_view, link
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import link
 from rest_framework.response import Response
 from simplepastebin.models import Snippet
 from simplepastebin.permissions import IsOwnerOrReadOnly
 from simplepastebin.serializers import SnippetSerializer, UserSerializer
 
 
-
-
-# class SnippetList(generics.ListCreateAPIView):
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#     
-#     def pre_save(self, obj):
-#         obj.owner = self.request.user
-# 
-# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-#                       IsOwnerOrReadOnly,)
-#     
-#     def pre_save(self, obj):
-#         obj.owner = self.request.user
-#         
-# class SnippetHighlight(generics.GenericAPIView):
-#     queryset = Snippet.objects.all()
-#     renderer_classes = (renderers.StaticHTMLRenderer,)
-# 
-#     def get(self, request, *args, **kwargs):
-#         snippet = self.get_object()
-#         return Response(snippet.highlighted) 
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
@@ -53,16 +27,6 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
     def pre_save(self, obj):
         obj.owner = self.request.user
-
-    
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-# 
-# 
-# class UserDetail(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
